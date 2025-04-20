43f7a5661acecf5f296e9e46238532c755c0043f
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-04-20 02:27:37 +0200

    added while loop

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,5 +1,34 @@
+package com.ferragnez.party;
+
+import java.util.Scanner;
+
 public class CheckGuest {
     public static void main(String[] args) {
+        Scanner name = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String userName = name.nextLine();
 
+        String[] guests = {
+                "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio",
+                "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"
+        };
+
+        boolean isOnGuestList = false;
+        int index = 0;
+
+        while (!isOnGuestList && index < guests.length) {
+            if (guests[index].equalsIgnoreCase(userName)) {
+                isOnGuestList = true;
+            }
+            index++;
+        }
+
+        if (isOnGuestList == true) {
+            System.out.println("Enjoy the party, " + userName + "!");
+        } else {
+            System.out.println("Sorry, " + userName + ", you're not on the guest list.");
+        }
+
+        name.close();
     }
 }
